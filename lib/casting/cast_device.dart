cb670c9648e1086c8948e845f829b4554cdc1d91
terrabythia <sander@frismedia.nl>
2019-01-06 12:57:36 +0100

    Now using eureka to get device's friendly name instead of device_info.xml

--- lib/casting/cast_device.dart
+++ lib/casting/cast_device.dart
@@ -1,5 +1,5 @@
 import 'package:http/http.dart' as http;
-import 'package:xml/xml.dart' as xml;
+import 'dart:convert';
 import 'package:observable/observable.dart';
 
 enum CastDeviceType {
@@ -26,17 +26,29 @@ class CastDevice extends ChangeNotifier {
 
     // fetch device info if possible
     if (CastDeviceType.ChromeCast == deviceType) {
-      http.get('http://${host}:8008/ssdp/device-desc.xml')
+      // for now we only request name, because we only use it to get the 'friendly name'
+      // Possible parameters: version,audio,name,build_info,detail,device_info,net,wifi,setup,settings,opt_in,opencast,multizone,proxy,night_mode_params,user_eq,room_equalizer
+      // see: https://rithvikvibhu.github.io/GHLocalApi/#device-info
+      http.get('http://${host}:8008/setup/eureka_info?params=name')
           .then((response) {
-        print("Response status: ${response.statusCode}");
-        print("Response body: ${response.body}");
-        xml.XmlDocument document = xml.parse(response.body);
-        xml.XmlElement deviceElement =
-            document.findElements('root').first
-            .findElements('device').first;
-        _friendlyName = deviceElement.findElements('friendlyName').first.text;
+        print("EUREKA: Response status: ${response.statusCode}");
+        print("EUREKA: Response body: ${response.body}");
+        Map deviceInfo = jsonDecode(response.body);
+        _friendlyName = deviceInfo['name'];
         notifyChange();
       });
+      // old way: xml from device-desc.xml
+//      http.get('http://${host}:8008/ssdp/device-desc.xml')
+//          .then((response) {
+//        print("Response status: ${response.statusCode}");
+//        print("Response body: ${response.body}");
+//        xml.XmlDocument document = xml.parse(response.body);
+//        xml.XmlElement deviceElement =
+//            document.findElements('root').first
+//            .findElements('device').first;
+//        _friendlyName = deviceElement.findElements('friendlyName').first.text;
+//        notifyChange();
+//      });
     }
     else if (CastDeviceType.AppleTV == deviceType) {
       // todo
